65ed767fdba9813034bb68e47c97986226267bb1
SukritiGoswami07 <sukriti.goswami@icloud.com>
2025-05-16 02:56:20 +0530

    Final Updated Code for Cost Lambda

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Variables
-LAMBDA_ROOT_DIR="./lambda/resource_cost_monitoring"
-STACK_NAME="ResourceCostMonitor"
-TEMPLATE_FILE="template.yaml"
-PARAM_FILE="parameters.json"
+LAMBDA_ROOT_DIR="./lambda/resource_cost_monitoring_lambda_deploy"
+STACK_NAME="peng-176-resource-cost-monitor-stack"
+TEMPLATE_FILE="./iac/template.yaml"
+PARAM_FILE="./iac/values/parameters.json"
 PACKAGE_SCRIPT="$LAMBDA_ROOT_DIR/package.sh"
-ZIP_FILE="$LAMBDA_ROOT_DIR/lambda_package.zip"
+ZIP_FILE="$LAMBDA_ROOT_DIR/lambda_package_123.zip"
 
 # Step 1: Run the packaging script
 if [ -f "$PACKAGE_SCRIPT" ]; then
   echo "Running packaging script..."
   chmod +x "$PACKAGE_SCRIPT"
-  ./"$PACKAGE_SCRIPT" "$LAMBDA_ROOT_DIR" "$ZIP_FILE"
+  ./"$PACKAGE_SCRIPT" $LAMBDA_ROOT_DIR $ZIP_FILE
   if [ $? -ne 0 ]; then
     echo "Error: Packaging script failed."
     exit 1
@@ -22,10 +22,36 @@ else
   exit 1
 fi
 
-if [ ! -f "$ZIP_FILE" ]; then
-  echo "Error: Lambda package ($ZIP_FILE) not found."
-  exit 1
+S3BucketName="peng-176-cost-monitoring-lambda-source-bucket"
+S3BucketDeployRegion="us-east-1"
+LambdaSourceFile="./lambda/lambda_source_file/config.yaml"
+
+if aws s3 ls $S3BucketName 2>&1 | grep -q 'NoSuchBucket'; then
+    echo "Creating S3 bucket."
+    aws s3api create-bucket --bucket $S3BucketName --region ${S3BucketDeployRegion}
+    else
+    echo "Bucket already exists..."
 fi
+
+echo "Copying Lambda source file into S3 bucket"
+
+aws s3 cp ${LambdaSourceFile} s3://${S3BucketName}/version-1/
+
+
+ROLE_ARN="arn:aws:iam::905418490146:role/POC-Execution-Role-ML-DS"
+SESSION_NAME="DeploySessionWithAssumeRole"
+
+echo "Assuming role: $ROLE_ARN"
+
+# Assume the role and capture temporary credentials
+CREDS=$(aws sts assume-role --role-arn "$ROLE_ARN" --role-session-name "$SESSION_NAME" --query 'Credentials' --output json)
+
+
+export AWS_ACCESS_KEY_ID=$(echo "$CREDS" | jq -r '.AccessKeyId')
+export AWS_SECRET_ACCESS_KEY=$(echo "$CREDS" | jq -r '.SecretAccessKey')
+export AWS_SESSION_TOKEN=$(echo "$CREDS" | jq -r '.SessionToken')
+
+echo "Starting deployment"
 
 if ! aws cloudformation describe-stacks --stack-name "$STACK_NAME" > /dev/null 2>&1; then
   echo "Stack does not exist. Creating stack..."
@@ -38,3 +64,5 @@ else
   aws cloudformation wait stack-update-complete --stack-name "$STACK_NAME"
   echo "Stack updated successfully."
 fi
+
+
